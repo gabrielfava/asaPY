77d2f981ce1f40f7d3d5567c807b0d8e71c048cc
Joao P. A. Mondoni <jp.mondoni@gmail.com>
2018-03-01 17:01:19 -0300

    Update conceitual do arquivo generate_pack
    Mapeamento das funções

--- Modulos/Packs/generate_pack.py
+++ Modulos/Packs/generate_pack.py
@@ -1,1 +1,35 @@
-#ASAPY
+#asaPY
+"""
+Geração de pacotes de exercícios prontos para ser importados ao BOCA.
+"""
+import zipfile
+
+__tests_in = []
+__tests_out = []
+
+
+def exercicio_form(ex):
+	"""
+	Valores de preenchimento do formulário do exercício:
+		* Título
+		* Autor
+		* Letra do Problema
+		* Corpo da questão
+		* Observações
+		* Casos de Teste
+	Retornar objeto com propriedades do exercício
+	"""
+	return ex
+
+def create_files(ex):
+	"""
+	Gerar arquivos do formulário e retornar um objeto com todos os arquivos que devem ser gerados no zip
+	"""
+	return files
+
+def generate_zip(files):
+	"""
+	Métodos para pegar os arquivos e juntar no pack aqui
+	"""
+
+
