9335915c4deb9732ef5eb66ec14912986e3c017c
Gabriel Fava <gabrielfavasouza@gmail.com>
2018-03-04 21:51:12 -0300

    files

---
+++ Modulos/Problemas/problema.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 21:17:17 2018
+
+@author: Gabriel Fava
+"""
+
+class problems(object):
+    ___ID       = -1;
+    ___title    = "";
+    
+    def setAutor(self,valor): 
+        self.___autor = valor;
+        
+    def setTitulo(self,valor): 
+        self.___titulo = valor;
